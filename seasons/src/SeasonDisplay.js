7a0c3d5d584d0114e18da1f5820a139ad040548c
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-06-28 16:50:43 +0200

    Add seasons project

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,36 @@
+import './SeasonDisplay.css'
+import React from 'react'
+
+const seasonConfig = {
+   summer: {
+      text: "Let's hit the beach!",
+      iconName: 'sun',
+   },
+   winter: {
+      text: 'Burr it is cold!',
+      iconName: 'snowflake',
+   },
+}
+
+const getSeason = (lat, month) => {
+   if (month > 2 && month < 9) {
+      return lat > 0 ? 'summer' : 'winter'
+   } else {
+      return lat > 0 ? 'winter' : 'summer'
+   }
+}
+
+const SeasonDisplay = ({ lat }) => {
+   const season = getSeason(lat, new Date().getMonth())
+   const { text, iconName } = seasonConfig[season]
+
+   return (
+      <div className={`${season} season-display`}>
+         <i className={`${iconName} icon massive icon-left`} />
+         <h1>{text}</h1>
+         <i className={`${iconName} icon massive icon-right`} />
+      </div>
+   )
+}
+
+export default SeasonDisplay
