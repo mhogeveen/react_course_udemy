410c7b400b7c239acd15fcb26b623ca6ff419574
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-07-01 14:52:44 +0200

    Other solution to overfetch issue (combined action creator)

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,13 @@
 import jsonPlaceholder from '../api/jsonPlaceholder'
 import _ from 'lodash'
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+   await dispatch(fetchPosts())
+
+   const userIds = _.uniq(_.map(getState().posts, 'userId'))
+   userIds.forEach((id) => dispatch(fetchUser(id)))
+}
+
 export const fetchPosts = () => async (dispatch) => {
    const response = await jsonPlaceholder.get('/posts')
 
@@ -10,13 +17,23 @@ export const fetchPosts = () => async (dispatch) => {
    })
 }
 
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch)
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
    const response = await jsonPlaceholder.get(`/users/${id}`)
 
    dispatch({
       type: 'FETCH_USER',
       payload: response.data,
    })
-})
+}
+
+// -- Memoized version of fetchUser action creator
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch)
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//    const response = await jsonPlaceholder.get(`/users/${id}`)
+
+//    dispatch({
+//       type: 'FETCH_USER',
+//       payload: response.data,
+//    })
+// })
